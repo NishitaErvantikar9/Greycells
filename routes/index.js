6b9a785024028516164f9e74cc0cef3ee04801da
shravya2468 <sandhya.tayade@gmail.com>
2023-05-26 09:54:13 +0530

    work in office

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'GreyCellsTech' });
 });
 
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'GreyCellsTech' });
+});
+router.get('/contact', function (req, res, next) {
+  res.render('contact', { title: 'GreyCellsTech' });
+});
+router.get('/customers', function (req, res, next) {
+  res.render('customers', { title: 'GreyCellsTech' });
+});
+
+router.get('/contract', function (req, res, next) {
+  res.render('customers', { title: 'GreyCellsTech' });
+});
 module.exports = router;
